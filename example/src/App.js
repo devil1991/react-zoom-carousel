b5a287ec4d9979f4a107791438e35b56557d1219
Karan Nijhawan <karan.n@barreldev.com>
2019-10-20 19:09:18 +0200

    Zoom gallery setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import ZoomGallery from 'react-zoom-carousel'
 
-import ExampleComponent from 'react-zoom-carousel'
+const images = [
+  'https://hogan.bordel.studio/images/detail-product/1_800x1100.png',
+  'https://hogan.bordel.studio/images/detail-product/2_800x1100.png',
+  'https://hogan.bordel.studio/images/detail-product/3_800x1100.png',
+  'https://hogan.bordel.studio/images/detail-product/4_800x1100.png',
+  'https://hogan.bordel.studio/images/detail-product/5_800x1100.png'
+]
 
 export default class App extends Component {
+  state = {
+    open: false,
+    initialIndex: 0
+  }
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ZoomGallery images={images} open={this.state.open} initialIndex={this.state.initialIndex} handleClose={() => this.setState({ open: false })} />
+        <button onClick={() => this.setState({ open: true })}>Toggle Zoom</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import PinchToZoom from 'react-responsive-pinch-zoom-pan'
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
+export default class ZoomGallery extends Component {
   static propTypes = {
-    text: PropTypes.string
+    images: PropTypes.array,
+    initialIndex: PropTypes.number,
+    open: PropTypes.bool,
+    handleClose: PropTypes.func
+  }
+
+  static defaultProps = {
+    initialIndex: 0,
+    images: [],
+    handleClose: () => null
+  }
+
+  constructor (props) {
+    super(props)
+    this.prevSlide = this.prevSlide.bind(this)
+    this.nextSlide = this.nextSlide.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.state = {
+      currentIndex: props.initialIndex,
+      loading: true
+    }
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (
+      this.state.currentIndex !== prevState.currentIndex ||
+      (this.props.open !== prevProps.open && this.props.open)
+    ) return this.handleLoading()
+  }
+
+  prevSlide () {
+    this.setState({ loading: true, currentIndex: this.state.currentIndex > 0 ? this.state.currentIndex - 1 : this.props.images.length - 1 })
+  }
+
+  nextSlide () {
+    this.setState({ loading: true, currentIndex: (this.state.currentIndex < this.props.images.length - 1) ? this.state.currentIndex + 1 : 0 })
+  }
+
+  handleClose () {
+    this.props.handleClose()
+  }
+
+  handleLoading () {
+    const image = new Image()
+    image.onload = () => {
+      this.setState({ loading: false })
+    }
+
+    image.src = this.props.images[this.state.currentIndex]
   }
 
   render() {
     const {
-      text
+      images = []
     } = this.props
-
+    if (!this.props.open || images.length === 0) return null
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={`${styles.wrapper} ZoomGalleryCarousel`}>
+        <PinchToZoom initialScale='auto' position='center' zoomButtons={false}>
+          <img className={`${styles.Image} ${this.state.loading ? styles.ImageLoading : ''}`} src={images[this.state.currentIndex]} />
+        </PinchToZoom>
+        { this.state.loading && <div className='spinner' /> }
+        <div className={styles.close} onClick={this.handleClose}>
+          <i className='svg-icon hogan-close' />
+        </div>
+        <div className={styles.footer}>
+          <span onClick={this.prevSlide} className={styles.action}><i className='icon-hogan-arrow-left icon' /></span>
+          <span className='status'>{`${this.state.currentIndex + 1}/${images.length}`}</span>
+          <span onClick={this.nextSlide} className={styles.action}><i className='icon-hogan-arrow-right icon' /></span>
+        </div>
       </div>
     )
   }
