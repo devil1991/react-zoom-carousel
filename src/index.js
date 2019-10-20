67c63b01daffbbff6512a503bafe6b5e65583ffb
Karan Nijhawan <karan.n@barreldev.com>
2019-10-20 20:32:05 +0200

    max width

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import PinchToZoom from 'react-responsive-pinch-zoom-pan'
+import { PinchView } from 'react-responsive-pinch-zoom-pan'
 import styles from './styles.css'
 
 export default class ZoomGallery extends Component {
@@ -67,9 +67,9 @@ export default class ZoomGallery extends Component {
     if (!this.props.open || images.length === 0) return null
     return (
       <div className={`${styles.wrapper} ZoomGalleryCarousel`}>
-        <PinchToZoom initialScale={1} position='center' zoomButtons={false} maxScale={4}>
+        <PinchView initialScale={1} position='center' zoomButtons={false} maxScale={4}>
           <img className={`${styles.Image} ${this.state.loading ? styles.ImageLoading : ''}`} src={images[this.state.currentIndex]} />
-        </PinchToZoom>
+        </PinchView>
         { this.state.loading && <div className='spinner' /> }
         <div className={styles.close} onClick={this.handleClose}>
           <i className='icon icon-hogan-close-menu' />
